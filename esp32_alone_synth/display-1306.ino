d6e3031f6df9b54bc9e1609b7dc3a43c1ffc718c
Frontier9 <48641859+MichaelPNolan@users.noreply.github.com>
2021-09-05 14:51:54 +0700

    Add files via upload

--- esp32_alone_synth/display-1306.ino
+++ esp32_alone_synth/display-1306.ino
@@ -26,7 +26,7 @@
 #include <Adafruit_SSD1306.h>
 
 #define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define SCREEN_HEIGHT 32 // OLED display height, in pixels
 
 
 #define OLED_RESET      -1// Reset pin # (or -1 if sharing Arduino reset pin)
